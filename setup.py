30e1df3547ca422b4ffb288e7751c417609539a6
nose1317 <karimshrif17@gmail.com>
2024-12-17 08:23:35 +0200

    Update setup.py with correct fields and classifiers

--- setup.py
+++ setup.py
@@ -5,20 +5,25 @@ setup(
     version="0.1.0",
     author="nose1317",
     description="A Python package for scraping, analyzing, and visualizing Last.fm music data.",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),  # Ensures correct encoding
     long_description_content_type="text/markdown",
-    packages=find_packages(),
+    url="https://github.com/nose1317/Last.fm-Music-Analytics",  # Add the project URL for reference
+    packages=find_packages(),  # This will automatically find all packages
     install_requires=[
         "playwright",
         "pandas",
         "matplotlib",
         "seaborn",
         "ipywidgets",
-        "ipython"
+        "ipython",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.7",  # Specifies that it requires Python 3.7 or higher
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",  # Specify the exact versions supported
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
