9b4f6b1730d72d5c222bc6f77870ea5214619e94
nose1317 <karimshrif17@gmail.com>
2024-12-17 08:49:48 +0200

    Update setup.py and add MANIFEST.in to include config.json and interactive notebook

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Last.fm-Music-Analytics",  
+    name="Last.fm-Music-Analytics",
     version="0.1.0",
     author="nose1317",
     description="A Python package for scraping, analyzing, and visualizing Last.fm music data.",
-    long_description=open("README.md", encoding="utf-8").read(),  # Ensures correct encoding
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/nose1317/Last.fm-Music-Analytics",  # Add the project URL for reference
-    packages=find_packages(),  # This will automatically find all packages
+    url="https://github.com/nose1317/Last.fm-Music-Analytics",
+    packages=find_packages(),
     install_requires=[
         "playwright",
         "pandas",
@@ -17,14 +17,18 @@ setup(
         "ipywidgets",
         "ipython",
     ],
-    python_requires=">=3.7",  # Specifies that it requires Python 3.7 or higher
+    python_requires=">=3.7",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",  # Specify the exact versions supported
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    package_data={
+        '': ['config.json', 'interactive_data_aggregator.ipynb'],
+    },
+    include_package_data=True,
 )
